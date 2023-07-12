999a0d5e1213fecd3c7a6f430cc5b4b8f2f13a56
Matthew Elgert <mdelgert@yahoo.com>
2023-07-11 21:10:59 -0400

    add samba example

---
+++ SambaAlpine/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set default values if parameters are not provided
+USERNAME=${1:-"default_user"}
+PASSWORD=${2:-"default_password"}
+
+# Add the user and set the password
+adduser -D -H -s /sbin/nologin $USERNAME
+echo -e "$PASSWORD\n$PASSWORD" | smbpasswd -a -s $USERNAME
+
+# Start the Samba server
+/usr/sbin/smbd --foreground
+
