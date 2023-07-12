29ee69e0def89d74f1296b7de5f8d5f821c97975
Matthew Elgert <mdelgert@yahoo.com>
2023-07-12 15:37:04 -0400

    update ssh alpine

--- SSH_Alpine/entrypoint.sh
+++ SSH_Alpine/entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 ssh-keygen -A
+
+adduser --disabled-password --gecos '' -h /home/$USERNAME -s /bin/sh -D $USERNAME && \
+    echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+chmod u+s /bin/su
+
+echo -n "$USERNAME:$PASSWORD" | chpasswd
+
+echo -n "root:$PASSWORD" | chpasswd
+
 exec /usr/sbin/sshd -D -e "$@"
