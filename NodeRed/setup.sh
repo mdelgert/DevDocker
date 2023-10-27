ada4d6b0e22166159eebb80c754cb8f3daac3f3c
Matthew Elgert <mdelgert@yahoo.com>
2023-10-27 19:25:07 -0400

    update setup

--- NodeRed/setup.sh
+++ NodeRed/setup.sh
@@ -4,4 +4,4 @@ docker rm node-red
 sudo rm -rf /mnt/d1/portainer/Files/AppData/Config/node-red
 sudo mkdir /mnt/d1/portainer/Files/AppData/Config/node-red
 sudo chmod 777 /mnt/d1/portainer/Files/AppData/Config/node-red
-docker run -d -it -p 85:1880 -v /mnt/d1/portainer/Files/AppData/Config/node-red:/data --name node-red nodered/node-red
+docker run -d -it -p 83:1880 -v /mnt/d1/portainer/Files/AppData/Config/node-red:/data --name node-red nodered/node-red
