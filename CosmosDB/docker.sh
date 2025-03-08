628edeaff5c18a82c5a2a6120613db27facff831
Matthew Elgert <mdelgert@yahoo.com>
2025-03-07 19:57:43 -0500

    Refactor MSSQL setup and documentation; remove old scripts and add new Docker configurations

--- CosmosDB/docker.sh
+++ CosmosDB/docker.sh
@@ -1,6 +1,8 @@
+docker cosmos-emulator
 docker run \
     --publish 8081:8081 \
     --publish 10250-10255:10250-10255 \
-    --name linux-emulator \
+    --name cosmos-emulator \
     --detach \
+    --restart=always \
     mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest
--- CosmosDB/notes.md
+++ CosmosDB/notes.md
@@ -1,1 +1,3 @@
 https://learn.microsoft.com/en-us/azure/cosmos-db/how-to-develop-emulator?tabs=docker-linux%2Ccsharp&pivots=api-nosql
+
+https://localhost:8081/_explorer/index.html
